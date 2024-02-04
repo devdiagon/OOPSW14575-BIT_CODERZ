c00513482451c1cfbb17b1cdfc81b7648c08c8ce
Frederick-Tipan <fredericktimo@gmial.com>
2024-02-03 20:49:48 -0500

    Organivent v1.14.2

---
+++ 06-Code/Organivent/src/main/java/ec/edu/espe/organivent/controller/AdministratorController.java
@@ -0,0 +1,15 @@
+package ec.edu.espe.organivent.controller;
+
+/**
+ *
+ * @author Frederick
+ */
+public class AdministratorController {
+    
+    public static boolean validateCredentials(String insertedUserName, String insertedPassword){
+        boolean passed = false;
+        
+        return passed;
+    }
+    
+}
